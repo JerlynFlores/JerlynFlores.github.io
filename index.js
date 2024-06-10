aa335a6e46a605a8c429350d6bef9354cad37df4
JerlynFlores <jerlynflores18@gmail.com>
2024-06-10 18:23:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const btnLike1 = document.getElementById("btnLike1")
+const countLike1 = document.getElementById("countLike1")
+
+function clickLike1(){
+  let totalLikes = parseInt(countLike1.value) + 1
+  countLike1.textContent = totalLikes.toString()
+}  
+btnLike1.addEventListener("click",clickLike1)
+
+const btnLike2 = document.getElementById("btnLike2")
+const countLike2 = document.getElementById("countLike2")
+
+function clickLike2(){
+  let totalLikes = parseInt(countLike2.value) + 1
+  countLike2.textContent = totalLikes.toString()
+}  
+btnLike2.addEventListener("click",clickLike2)
+
 const btnDisLike1 = document.getElementById("btnDisLike1")
 const countDisLike1 = document.getElementById("countDisLike1")
 
@@ -15,3 +33,17 @@ function clickDisLike2(){
   countDisLike2.textContent = totalDisLikes.toString()
 }  
 btnDisLike2.addEventListener("click",clickDisLike2)
+
+const submit = document.getElementById("submit")
+
+const comment = document.getElementById("comment")
+
+const commentbox = document.getElementById("commentbox")
+
+function submitComment(){
+
+commentbox.textContent += comment.value.toString() + "\n"
+comment.value=""
+}
+
+submit.addEventListener("click", submitComment)
